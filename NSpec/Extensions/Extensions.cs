3b7123235540a5ff462b7af5705e2ad65563fdae
Amir Rajan <ar@amirrajan.net>
2011-03-05 16:29:21 -0600

    changed tuples namespace to NSpec.Domain

--- NSpec/Extensions/Extensions.cs
+++ NSpec/Extensions/Extensions.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using NSpecSpec;
+using NSpec.Domain;
 
 namespace NSpec.Extensions
 {
--- SampleSpecs/Demo/prime_kata.cs
+++ SampleSpecs/Demo/prime_kata.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using NSpec.Extensions;
 using NSpec.Interpreter.Indexer;
-using NSpecSpec;
+using NSpec.Domain;
 
 namespace SampleSpecs.Demo
 {
