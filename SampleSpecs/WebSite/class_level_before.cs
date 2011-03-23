111a5ee5538c69fcae2be29248e9e9b22f54a032
Amir Rajan <ar@amirrajan.net>
2011-03-23 18:07:57 -0500

    added icon, started class level before

---
+++ SampleSpecs/WebSite/class_level_before.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NSpec;
+
+namespace SampleSpecs.WebSite
+{
+    class class_level_before : nspec
+    {
+        List<int> ints = null;
+
+        before<dynamic> each = (s) =>
+        {
+            s.ints = new List<int>();
+        };
+
+        public void list_manipulations()
+        {
+            specify = () => ints.Count.should_be(0);
+
+            context["number in collection"] = () =>
+            {
+                before = () => ints.Add(15);
+
+                specify = () => ints.Count.should_be(1);
+            };
+        }
+    }
+}
