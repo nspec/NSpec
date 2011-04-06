9b6bc63f941af37ded81dff2119e34f0cf4081b2
mattflo <matt@mattflo.com>
2011-04-06 08:49:47 -0500

    changed act example to batman sounds as text

---
+++ SampleSpecs/WebSite/describe_batman_sound_effects_as_text.cs
@@ -0,0 +1,24 @@
+﻿using NSpec;
+
+class describe_batman_sound_effects_as_text : nspec
+{
+    void they_are_loud_and_emphatic()
+    {
+        //allows you to declare a common "act" as in
+        //arrange - act - assert for all subcontexts
+        act = () => sound = sound.ToUpper() + "!!!";
+        context["given bam"] = () =>
+        {
+            before = () => sound = "bam";
+            it["should be BAM!!!"] = 
+                () => sound.should_be("BAM!!!");
+        };
+        context["given whack"] = () =>
+        {
+            before = () => sound = "whack";
+            it["should be WHACK!!!"] = 
+                () => sound.should_be("WHACK!!!");
+        };
+    }
+    string sound;
+}
