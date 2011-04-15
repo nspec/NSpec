ce91c767716d4641d6281f6dbdf30d7803a1e197
mattflo <matt@mattflo.com>
2011-04-14 23:00:20 -0500

    new subcontext registered item

--- SampleSpecs/Compare/NSpec/VendingMachineSpec.cs
+++ SampleSpecs/Compare/NSpec/VendingMachineSpec.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using NSpec;
 
 namespace SampleSpecs.Compare.NSpec
@@ -10,16 +11,34 @@ namespace SampleSpecs.Compare.NSpec
             before = () => machine = new VendingMachine();
 
             it["should have no items"] = ()=> machine.Items().should_be_empty();
+
+            context["given doritos are registered in A1 for 50 cents"] = () =>
+            {
+                before = () => machine.RegisterItem("A1", "doritos", .5m);
+
+                specify = () => machine.Items().Count().should_be(1);
+            };
         }
         private VendingMachine machine;
     }
 
     internal class VendingMachine
     {
+        public VendingMachine()
+        {
+            items = new Item[] { };
+        }
+
         public IEnumerable<Item> Items()
         {
-            return new Item[] { };
+            return items;
         }
+
+        public void RegisterItem(string slot, string name, decimal price)
+        {
+            items = new[]{new Item()};
+        }
+        private Item[] items;
     }
 
     internal class Item
