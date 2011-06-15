4c905a0cdfb5d328d626030563429f656ea0caaa
jgill13 <jg_git@thegillhousehold.com>
2011-06-14 23:22:12 -0500

    Updated the CSS for the HtmlFormatter

--- NSpec/Domain/Formatters/HtmlFormatter.cs
+++ NSpec/Domain/Formatters/HtmlFormatter.cs
@@ -16,24 +16,28 @@ namespace NSpec.Domain.Formatters
             sb.AppendLine( "<html>" );
             sb.AppendLine( "<head>" );
             sb.AppendLine( "<style type=\"text/css\">" );
-            sb.AppendLine( "  span {padding: 1em}" );
-            sb.AppendLine( "  .results {background-color: #FFFFCC; border-color: #000000; border-style: dashed; border-width: thin; font-weight:bold; white-space: pre-wrap; text-align: center;}" );
-            sb.AppendLine( "  .context-parent-name {font-weight:bold;}" );
-            sb.AppendLine( "  .spec-passed {font-weight:bold; color:green;}" );
-            sb.AppendLine( "  .spec-failed {font-weight:bold; color:#FF0000;}" );
-            sb.AppendLine( "  .spec-pending {font-weight:bold; color:#FF6600;}" );
-            sb.AppendLine( "  .spec-exception {background-color: #FFD2CF; border-color: #FF828D; border-style: dashed; border-width: thin; padding: 1em; white-space: pre-wrap;}" );
-            sb.AppendLine( "  .runDate {color:grey; font-size:x-small;}" );
+			sb.AppendLine( "  span {padding: 0.5em}" );
+			sb.AppendLine( "  ul {margin-top: 0px}" );
+			sb.AppendLine( "  .results {background-color: #FFFFCC; border-color: #000000; border-style: dashed; border-width: thin; font-weight:bold; white-space: pre-wrap; text-align: center; padding: 0.5em}" );
+			sb.AppendLine( "  .context-parent {border-color: #000000; border-style: dashed; border-width: thin; margin-bottom: 1em;}" );
+			sb.AppendLine( "  .context-parent-title {background-color: #6699FF; border-color: #000000; border-bottom-style: dashed; border-width: thin; font-weight:bold; padding: 0.5em}" );
+			sb.AppendLine( "  .context-parent-body {padding: 1em 2em 1em 0;}" );
+			sb.AppendLine( "  .context-parent-name {font-weight:bold;}" );
+			sb.AppendLine( "  .spec-passed {font-weight:bold; color:green;}" );
+			sb.AppendLine( "  .spec-failed {font-weight:bold; color:#FF0000;}" );
+			sb.AppendLine( "  .spec-pending {font-weight:bold; color:#0000FF;}" );
+			sb.AppendLine( "  .spec-exception {background-color: #FFD2CF; border-color: #FF828D; border-style: dashed; border-width: thin; padding: 1em; font-size:small; white-space: pre-wrap; padding: 0.5em}" );
+			sb.AppendLine( "  .run-date {color:grey; font-size:x-small;}" );
             sb.AppendLine( "</style>" );
             sb.AppendLine( "</head>" );
             sb.AppendLine( "<body>" );
 
             sb.AppendLine();
             sb.Append( "<div class=\"results\">" );
-            sb.AppendFormat( "Specs: <span>{0}</span>", contexts.Examples().Count() ); 
-            sb.AppendFormat( "Failed: <span class=\"spec-failed\">{0}</span>", contexts.Failures().Count() ); 
-            sb.AppendFormat( "Pending: <span class=\"spec-pending\">{0}</span>", contexts.Pendings().Count() ); 
-            sb.AppendFormat( "<div class=\"runDate\">Run Date: {0}</div>", DateTime.Now ); 
+            sb.AppendFormat( "Specs:<span>{0}</span>", contexts.Examples().Count() ); 
+            sb.AppendFormat( "Failed:<span class=\"spec-failed\">{0}</span>", contexts.Failures().Count() ); 
+            sb.AppendFormat( "Pending:<span class=\"spec-pending\">{0}</span>", contexts.Pendings().Count() ); 
+            sb.AppendFormat( "<div class=\"run-date\">Run Date: {0}</div>", DateTime.Now ); 
             sb.Append( "</div>" );
             sb.AppendLine();
             sb.AppendLine( "<br />" );
@@ -47,10 +51,13 @@ namespace NSpec.Domain.Formatters
         }
         void BuildParentContext( StringBuilder sb, Context context )
         {
-            sb.AppendLine( "<div class=\"parent-context\">" );
-            sb.AppendFormat( "  <span class=\"context-parent-name\">{0}</span>", context.Name );
+            sb.AppendLine( "<div class=\"context-parent\">" );
+            sb.AppendFormat( "  <div class=\"context-parent-title\">{0}</div>", context.Name );
+            sb.AppendLine();
+            sb.AppendFormat( "    <div class=\"context-parent-body\">" );
             sb.AppendLine();
             context.Contexts.Do( c => this.BuildChildContext( sb, c ) );
+            sb.AppendLine( "  </div>" );
             sb.AppendLine( "</div>" );
         }
 
