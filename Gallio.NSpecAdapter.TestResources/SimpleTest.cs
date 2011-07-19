d4ba5567f45fa462844dfee5dd194ebb00ca4275
jgill13 <jg_git@thegillhousehold.com>
2011-07-19 12:37:42 -0500

    Created a TestResources project and put a couple of simple NSpec tests in it.

---
+++ Gallio.NSpecAdapter.TestResources/SimpleTest.cs
@@ -0,0 +1,26 @@
+using NSpec;
+
+namespace Gallio.NSpecAdapter.TestResources
+{
+    /// <summary>
+    /// A simple test fixture
+    /// </summary>
+    class A_simple_test : nspec
+    {
+        /// <summary>
+        /// A passing test
+        /// </summary>
+        void This_test_should_pass()
+        {
+            it["Yeah, it passes!"] = () => true.should_be_true();
+        }
+
+        /// <summary>
+        /// A failing test
+        /// </summary>
+        void This_test_should_fail()
+        {
+            it["Yeah, it failed as expected."] = () => true.should_be_false();
+        }
+    }
+}
