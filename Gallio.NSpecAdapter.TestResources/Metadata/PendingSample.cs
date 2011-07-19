e58d192eb53f68a5c02ef15aec8b8f31ee6c87f4
jgill13 <jg_git@thegillhousehold.com>
2011-07-19 12:37:42 -0500

    Created a TestResources project and put a couple of simple NSpec tests in it.

---
+++ Gallio.NSpecAdapter.TestResources/Metadata/PendingSample.cs
@@ -0,0 +1,19 @@
+using NSpec;
+
+namespace Gallio.NSpecAdapter.TestResources.Metadata
+{
+    /// <summary>
+    /// A simple test fixture
+    /// </summary>
+    class A_pending_test : nspec
+    {
+        /// <summary>
+        /// A pending test
+        /// </summary>
+        void These_tests_are_pending()
+        {
+            it["I am pending"] = todo;
+            xit["I am also pending"] = () => "".should_be( "Blah" );;
+        }
+    }
+}
