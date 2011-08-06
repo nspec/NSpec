6e2bb61cc4e442a41b557f6429802ebe9c925025
Amir Rajan <ar@amirrajan.net>
2011-08-06 18:14:34 -0500

    added nspec debugger shim

---
+++ SampleSpecs/DebuggerShim.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using NUnit.Framework;
+using NSpec.Domain;
+using System.Reflection;
+using NSpec;
+using NSpec.Domain.Formatters;
+
+[TestFixture]
+public class DebuggerShim
+{
+    [Test]
+    public void debug()
+    {
+        //the specification class you want to test
+        //this can be a regular expression
+        var testClassYouWantToDebug = "describe_SomeTest";
+
+        //initialize NSpec's specfinder
+        var finder = new SpecFinder(
+            Assembly.GetExecutingAssembly().Location,
+            new Reflector(),
+            testClassYouWantToDebug);
+
+        //initialize NSpec's builder
+        var builder = new ContextBuilder(
+            finder,
+            new DefaultConventions());
+
+        //this line runs the tests you specified in the filter
+        new ContextRunner(builder, new ConsoleFormatter()).Run();
+    }
+}
