5ad7c1e8e9f3dd2bbc37708679600876b6d68dac
Matt Florence <matt@mattflo.com>
2011-10-04 21:31:32 -0500

    trash

---
+++ NSpecSpecs/describe_RunningSpecs/describe_class_level_after.cs
@@ -0,0 +1,49 @@
+﻿using System.Linq;
+using NSpecSpecs.WhenRunningSpecs;
+using NSpec;
+using NUnit.Framework;
+
+namespace NSpecSpecs.describe_RunningSpecs
+{
+    [TestFixture]
+    [Category("RunningSpecs")]
+    public class describe_class_level_after : when_running_specs
+    {
+        class SpecClass : nspec
+        {
+            public string ExecutionSequence;
+
+            void after_each()
+            {
+                ExecutionSequence += "B";
+            }
+        }
+
+        class DerivedClass : SpecClass
+        {
+            void after_each()
+            {
+                ExecutionSequence = "A";
+            }
+
+            void running_example()
+            {
+                it["works"] = () => ExecutionSequence.should_be(null);
+            }
+        }
+
+        [Test]
+        public void afters_are_run_in_the_correct_order()
+        {
+            Run(typeof(DerivedClass));
+
+            var specInstance = classContext.GetInstance() as DerivedClass;
+
+            var executionSequence = specInstance.ExecutionSequence;
+
+            executionSequence.should_be("AB");
+
+            classContext.Failures().Count().should_be(0);
+        }
+    }
+}
---
+++ SampleSpecs/Bug/describe_after.cs
@@ -0,0 +1,29 @@
+﻿using NSpec;
+
+namespace SampleSpecs.Bug
+{
+    class describe_after : nspec
+    {
+        string sequence = "";
+
+        void before_each()
+        {
+            sequence += "1";
+        }
+
+        void it_is_one()
+        {
+            sequence.Is("1");
+        }
+
+        void it_is_still_just_one()
+        {
+            sequence.Is("1");
+        }
+
+        void after_each()
+        {
+            sequence = "";
+        }
+    }
+}
