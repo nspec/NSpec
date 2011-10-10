5154a2946ca1d8fe987ac6714d5d437439de8815
Jonathan Boarman <jb@jonathan.net>
2011-10-10 18:08:02 -0500

    Ensuring exception thrown by 'act' is never reached when exception is thrown by 'before'.

--- NSpecSpecs/describe_RunningSpecs/when_before_contains_exception.cs
+++ NSpecSpecs/describe_RunningSpecs/when_before_contains_exception.cs
@@ -19,7 +19,14 @@ namespace NSpecSpecs.WhenRunningSpecs
                 it["should fail this example because of before"] = () => "1".should_be("1");
 
                 it["should also fail this example because of before"] = () => "1".should_be("1");
-            }
+
+                context[ "exception thrown by both before and act" ] = () =>
+                {
+                    act = () => { throw new ArgumentException( "this exception should never be thrown" ); };
+
+                    it[ "tracks only the first exception from 'before'" ] = () => "1".should_be( "1" );
+                };
+			}
         }
 
         [SetUp]
@@ -33,6 +40,12 @@ namespace NSpecSpecs.WhenRunningSpecs
         {
             TheExample("should fail this example because of before").Exception.GetType().should_be(typeof(InvalidOperationException));
             TheExample("should also fail this example because of before").Exception.GetType().should_be(typeof(InvalidOperationException));
+        }
+
+        [Test]
+        public void it_should_throw_exception_from_before_not_from_act()
+        {
+            TheExample( "tracks only the first exception from 'before'" ).Exception.GetType().should_be( typeof( InvalidOperationException ) );
         }
 
         private Example TheExample(string name)
