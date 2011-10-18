fa27f1e5bd022ec996beff5ef6784a6c1052f30c
Jonathan Boarman <jb@jonathan.net>
2011-10-18 17:34:01 -0500

    Add support for runner to use tags filter passed on command line.

---
+++ NSpec/Domain/Tags.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace NSpec.Domain
+{
+	public class Tags
+	{
+		public List< string > IncludeTags = new List<string>();
+		public List< string > ExcludeTags = new List<string>();
+
+		/// <summary>Parses a string containing tags into a collection of normalized tags</summary>
+		public static List<string> ParseTags( string tags )
+		{
+			var tagsCollection = new List<string>();
+
+			// store one or more tags delimited by either commas or spaces
+			if( !string.IsNullOrEmpty( tags ) )
+			{
+				foreach( var tag in tags.Split( new[] { ',', ' ' } ) )
+				{
+					// store tags without any leading ampersat in the tag (e.g., '@mytag' is stored as 'mytag')
+					var rawTag = tag.TrimStart( new[] { '@' } );
+					if( !string.IsNullOrEmpty( rawTag ) )
+						tagsCollection.Add( rawTag );
+				}
+			}
+
+			return tagsCollection;
+		}
+
+		/// <summary>Parses a string containing tag filters into tag filter collections</summary>
+		public static void ParseTagFilters( string tags, ref List<string> includeTags, ref List<string> excludeTags )
+		{
+			if( includeTags == null )
+				includeTags = new List<string>();
+			if( excludeTags == null )
+				excludeTags = new List<string>();
+
+			// store one or more tags delimited by either commas or spaces
+			if( !string.IsNullOrEmpty( tags ) )
+			{
+				foreach( var tag in tags.Split( new[] { ',', ' ' } ) )
+				{
+					// determine whether tag is an include or exclude filter
+					List< string > targetTagCollection = tag.StartsWith( "~" ) ? excludeTags : includeTags;
+
+					// store tags without any leading ampersat in the tag (e.g., '@mytag' is stored as 'mytag')
+					var rawTag = tag.TrimStart( new[] { '~', '@' } );
+					if( !string.IsNullOrEmpty( rawTag ) )
+						targetTagCollection.Add( rawTag );
+				}
+			}
+		}
+
+		/// <summary>Parses a string containing tag filters into the internal tag filter collections</summary>
+		public Tags ParseTagFilters( string tags )
+		{
+			ParseTagFilters( tags, ref IncludeTags, ref ExcludeTags );
+
+			return this;
+		}
+
+		public bool Includes( string tag )
+		{
+			return IncludeTags.Contains( tag.TrimStart( new[] { '@' } ) );
+		}
+
+		public bool IncludesAny( List<string> tags )
+		{
+			return IncludeTags.Intersect( tags ).Any();
+		}
+
+		public bool Excludes( string tag )
+		{
+			return ExcludeTags.Contains( tag.TrimStart( new[] { '@' } ) );
+		}
+
+		public bool ExcludesAny( List<string> tags )
+		{
+			return ExcludeTags.Intersect( tags ).Any();
+		}
+	}
+}
---
+++ NSpecSpecs/describe_RunningSpecs/describe_tags.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NSpec;
+using NSpec.Domain;
+using NUnit.Framework;
+
+namespace NSpecSpecs.WhenRunningSpecs
+{
+	[TestFixture]
+	[Category( "RunningSpecs" )]
+	public class describe_tags : when_running_specs
+	{
+		class SpecClass : nspec
+		{
+			public void parses_tags()
+			{
+				it[ "parses single tag" ] = () =>
+				{
+					var tags = Tags.ParseTags( "mytag" );
+					tags.should_contain_tag( "mytag" );
+				};
+
+				it[ "parses ampersat tag" ] = () =>
+				{
+					var tags = Tags.ParseTags( "@mytag" );
+					tags.should_contain_tag( "mytag" );
+				};
+
+				it[ "parses multiple tags" ] = () =>
+				{
+					var tags = Tags.ParseTags( "myTag1,@myTag2" );
+					tags.should_contain_tag( "@myTag1" );
+					tags.should_contain_tag( "myTag2" );
+					tags.Count.should_be( 2 );
+				};
+			}
+
+			public void parses_tag_filters()
+			{
+				it[ "parses single 'include' tag filter" ] = () =>
+				{
+					var tags = new Tags();
+					tags.ParseTagFilters( "mytag" );
+					tags.should_tag_as_included( "mytag" );
+				};
+
+				it[ "parses single 'exclude' tag filter" ] = () =>
+				{
+					var tags = new Tags();
+					tags.ParseTagFilters( "~mytag" );
+					tags.should_tag_as_excluded( "mytag" );
+				};
+
+				it[ "parses ampersat tag filter" ] = () =>
+				{
+					var tags = new Tags();
+					tags.ParseTagFilters( "@mytag" );
+					tags.should_tag_as_included( "mytag" );
+				};
+
+				it[ "parses multiple tags filter" ] = () =>
+				{
+					var tags = new Tags();
+					tags.ParseTagFilters( "myInclude1,~myExclude1,@myInclude2,~@myExclude2," );
+					tags.should_tag_as_excluded( "@myExclude1" );
+					tags.should_tag_as_excluded( "myExclude2" );
+					tags.should_tag_as_included( "@myInclude1" );
+					tags.should_tag_as_included( "myInclude2" );
+					tags.IncludeTags.Count.should_be( 2 );
+					tags.ExcludeTags.Count.should_be( 2 );
+				};
+			}
+		}
+
+		[SetUp]
+		public void setup()
+		{
+			Run( typeof( SpecClass ) );
+		}
+
+		[Test]
+		public void parses_single_tag()
+		{
+			TheExample( "parses single tag" ).ExampleLevelException.should_be( null );
+		}
+
+		[Test]
+		public void parses_ampersat_tag()
+		{
+			TheExample( "parses ampersat tag" ).ExampleLevelException.should_be( null );
+		}
+
+		[Test]
+		public void parses_multiple_tags()
+		{
+			TheExample( "parses multiple tags" ).ExampleLevelException.should_be( null );
+		}
+
+		[Test]
+		public void parses_single_include_tag_filters()
+		{
+			TheExample( "parses single 'include' tag filter" ).ExampleLevelException.should_be( null );
+		}
+
+		[Test]
+		public void parses_single_exclude_tag_filters()
+		{
+			TheExample( "parses single 'exclude' tag filter" ).ExampleLevelException.should_be( null );
+		}
+
+		[Test]
+		public void parses_ampersat_tag_filters()
+		{
+			TheExample( "parses ampersat tag filter" ).ExampleLevelException.should_be( null );
+		}
+
+		[Test]
+		public void parses_multiple_tags_filters()
+		{
+			TheExample( "parses multiple tags filter" ).ExampleLevelException.should_be( null );
+		}
+
+		private Example TheExample( string name )
+		{
+			return classContext.AllContexts().SelectMany( context => context.Examples.Where( example => example.Spec == name ) ).FirstOrDefault();
+		}
+	}
+}
