7960008c572ecf23e8ed4fdbb04d71edf180379c
Matt Florence <matt@mattflo.com>
2012-02-21 22:23:28 -0600

    adding greater than demo spec

---
+++ SampleSpecs/Demo/describe_greater_than.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using NSpec;
+
+namespace SampleSpecs.Demo
+{
+    class describe_greater_than :nspec
+    {
+        void it_evaluates_passing_examples_correctly()
+        {
+            3.should_be_greater_than(2);
+        }
+
+        void it_evaluates_failing_examples_correctly()
+        {
+            expect<Exception>(() => 1.should_be_greater_than(1));
+        }
+    }
+}
