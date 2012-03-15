d3d28f299770a5f74f200bb8cb8e2018d88833fc
Amir Rajan <ar@amirrajan.net>
2012-03-15 18:48:57 -0500

    changed test for console output verification to just be a row test

--- NSpecSpecs/describe_Output/when_run_by_NSpecRunner.cs
+++ NSpecSpecs/describe_Output/when_run_by_NSpecRunner.cs
@@ -1,11 +1,26 @@
 using System.Diagnostics;
 using System.IO;
 using System.Reflection;
+using NUnit.Framework;
+using System;
+using NSpec;
 
 namespace NSpecSpecs.describe_Output
 {
+    [TestFixture]
     public class when_run_by_NSpecRunner 
     {
+        [Test, 
+        TestCase(typeof(describe_before_expected))]
+        public void output_verification(Type output)
+        {
+            var expectedOutput = output.GetField("Output").GetValue(output);
+
+            var tag = output.Name.Replace("_expected", "");
+
+            Run(tag).Is(expectedOutput);
+        }
+
         public string Run(string tag)
         {
             var process = new Process();
