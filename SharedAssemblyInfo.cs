c71c50502183a742fbf6fa85c383cb137b89b10f
Matt Florence <mflorence@onetechnologies.net>
2012-04-24 09:32:32 -0500

    better assembly resolution and less aggressive stacktrace trimming

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -21,5 +21,5 @@
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.9.59")]
-[assembly: AssemblyFileVersion("0.9.59")]
+[assembly: AssemblyVersion("0.9.60")]
+[assembly: AssemblyFileVersion("0.9.60")]
