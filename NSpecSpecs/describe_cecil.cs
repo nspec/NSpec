0ac5f93db8b4a68a02e0168ac53d05a0fd01f40f
Matt Florence <mflorence@onetechnologies.net>
2012-05-02 21:05:34 -0500

    playing around with cecil and enabled nuget package restore/build foo

---
+++ NSpecSpecs/describe_cecil.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using Mono.Cecil;
+using NUnit.Framework;
+
+namespace NSpecSpecs
+{
+    [TestFixture]
+    public class describe_cecil
+    {
+        [Test]
+        public void NAME()
+        {
+            var def = AssemblyDefinition.ReadAssembly(@"C:\Projects\NSpec\NSpecSpecs\bin\Debug\SampleSpecs.dll");
+
+            var before = def.MainModule.Types.First(t => t.Name == "describe_before");
+
+            before.Methods.ToList().ForEach(Console.WriteLine);
+        }
+    }
+}
