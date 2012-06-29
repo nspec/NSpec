4043b8d780d3dc034f51438038ebe46541f4a9c0
Matt Florence <mflorence@onetechnologies.net>
2012-06-29 09:08:49 -0500

    sorting out middle abstract class oddities

--- NSpecSpecs/describe_RunningSpecs/describe_class_level_after.cs
+++ NSpecSpecs/describe_RunningSpecs/describe_class_level_after.cs
@@ -11,6 +11,10 @@ namespace NSpecSpecs.describe_RunningSpecs
     {
         class BaseClass : sequence_spec
         {
+            void after_all()
+            {
+                sequence += "C";
+            }
             void after_each()
             {
                 sequence += "B";
@@ -68,19 +72,36 @@ namespace NSpecSpecs.describe_RunningSpecs
 
             Run(typeof(DerivedClass1));
             
-            DerivedClass1.sequence.Is("1AB");
+            DerivedClass1.sequence.Is("1ABC");
         }
 
         [Test]
-        public void afters_are_run_in_the_correct_order_when_abstract_middle_classes_are_present()
+        public void filtering_for_only_DerivedClass5()
         {
             DerivedClass5.sequence = "";
 
-            tags = typeof (DerivedClass5).Name;
+            tags = typeof(DerivedClass5).Name;
 
             Run(typeof(DerivedClass5));
 
-            DerivedClass5.sequence.Is("2789AB");
+            DerivedClass5.sequence.Is("2789ABC");
+        }
+
+        [Test,Ignore("intermittent every other failure with ncrunch")]
+        public void without_filtering()
+        {
+            DerivedClass5.sequence = "";
+
+            Run(typeof(DerivedClass5));
+
+            //you might expect A and B run just once
+            //DerivedClass5.sequence.Is("789AB");
+
+            //but when the tags filter is not in play it causes A and B run more than expected.
+            //this is because both "running examples" run one from DerivedClass5 and one from 
+            //DerivedClass1. And since A and B are after each, they run for each example
+
+            DerivedClass5.sequence.Is("1AB2789ABC");
         }
     }
 }
