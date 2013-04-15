d1678c7b6691ec7d2511330ba070ba2c1913a816
Matt Florence <mflorence@onetechnologies.net>
2013-04-15 12:45:32 -0500

    0.9.65

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -21,5 +21,5 @@
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.9.64")]
-[assembly: AssemblyFileVersion("0.9.64")]
+[assembly: AssemblyVersion("0.9.65")]
+[assembly: AssemblyFileVersion("0.9.65")]
