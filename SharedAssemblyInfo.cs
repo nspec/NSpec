5fc9d5173193f5eba14e4ed9974b643ae171a64e
Amir Rajan <ar@amirrajan.net>
2013-07-03 11:49:27 -0500

    added focus tag to run a specific test over and over (overrides what's passed in the command line), fixed console colors after test run

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -21,5 +21,5 @@
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.9.65")]
-[assembly: AssemblyFileVersion("0.9.65")]
+[assembly: AssemblyVersion("0.9.67")]
+[assembly: AssemblyFileVersion("0.9.67")]
