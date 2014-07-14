02d96f6eaadc5971728543623d669b2f322a0775
Michael Dripps <michael.dripps@gmail.com>
2014-07-14 14:53:58 +0100

    Added VB.NET NSpec Specs project to solution with one passing test

---
+++ NSpecSpecsVB/Describe_RunningSpecs/Describe_Example.vb
@@ -0,0 +1,39 @@
+﻿Imports NSpec
+Imports NUnit.Framework
+Imports NSpecSpecs.WhenRunningSpecs
+
+Namespace WhenRunningSpecs
+
+    <TestFixture()>
+    <Category("RunningSpecs")>
+    Public Class DescribeExample
+        Inherits when_running_specs
+
+        Public Class SpecClass
+            Inherits NSpec.nspec
+
+            Sub It_Changes_Status_After_Run()
+
+            End Sub
+
+            Sub It_Passes()
+
+            End Sub
+
+            Sub It_Fails()
+                Throw New Exception()
+            End Sub
+        End Class
+
+        <Test()>
+        Sub Execution_Status_Changes_After_Run()
+            Run(GetType(SpecClass))
+
+            Dim ex = TheExample("It Changes Status After Run")
+
+            ex.HasRun.should_be_true()
+        End Sub
+    End Class
+
+End Namespace
+
