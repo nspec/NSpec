eb51b88363703233a3174023d7b58b938457a220
Giuseppe Piscopo <g.piscopo@braincrumbz.com>
2015-08-30 14:58:21 +0200

    Extend coverage on asyncBefore exceptions

--- NSpecSpecs/describe_RunningSpecs/Exceptions/when_async_before_contains_exception.cs
+++ NSpecSpecs/describe_RunningSpecs/Exceptions/when_async_before_contains_exception.cs
@@ -22,15 +22,15 @@ namespace NSpecSpecs.describe_RunningSpecs.Exceptions
                     throw new InvalidOperationException(); 
                 };
 
-                it["should fail this example because of before"] = () => "1".should_be("1");
+                it["should fail this example because of asyncBefore"] = () => "1".should_be("1");
 
-                it["should also fail this example because of before"] = () => "1".should_be("1");
+                it["should also fail this example because of asyncBefore"] = () => "1".should_be("1");
 
-                context["exception thrown by both before and act"] = () =>
+                context["exception thrown by both asyncBefore and act"] = () =>
                 {
                     act = () => { throw new ArgumentException("this exception should never be thrown"); };
 
-                    it["tracks only the first exception from 'before'"] = () => "1".should_be("1");
+                    it["tracks only the first exception from 'asyncBefore'"] = () => "1".should_be("1");
                 };
             }
         }
@@ -44,27 +44,27 @@ namespace NSpecSpecs.describe_RunningSpecs.Exceptions
         [Test]
         public void the_example_level_failure_should_indicate_a_context_failure()
         {
-            TheExample("should fail this example because of before")
+            TheExample("should fail this example because of asyncBefore")
                 .Exception.GetType().should_be(typeof(ExampleFailureException));
-            TheExample("should also fail this example because of before")
+            TheExample("should also fail this example because of asyncBefore")
                 .Exception.GetType().should_be(typeof(ExampleFailureException));
-            TheExample("tracks only the first exception from 'before'")
+            TheExample("tracks only the first exception from 'asyncBefore'")
                 .Exception.GetType().should_be(typeof(ExampleFailureException));
         }
 
         [Test]
-        public void it_should_fail_all_examples_in_before()
+        public void it_should_fail_all_examples_in_async_before()
         {
-            TheExample("should fail this example because of before")
+            TheExample("should fail this example because of asyncBefore")
                 .Exception.InnerException.GetType().should_be(typeof(InvalidOperationException));
-            TheExample("should also fail this example because of before")
+            TheExample("should also fail this example because of asyncBefore")
                 .Exception.InnerException.GetType().should_be(typeof(InvalidOperationException));
         }
 
         [Test]
-        public void it_should_throw_exception_from_before_not_from_act()
+        public void it_should_throw_exception_from_async_before_not_from_act()
         {
-            TheExample("tracks only the first exception from 'before'")
+            TheExample("tracks only the first exception from 'asyncBefore'")
                 .Exception.InnerException.GetType().should_be(typeof(InvalidOperationException));
         }
     }
