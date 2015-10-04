59bd0098054bf4735f5e78dabbac14bfbafec4bf
Amir Rajan <ar@amirrajan.net>
2015-10-04 17:01:39 -0600

    1.0 release. it'll work this time i'm sure

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -21,5 +21,5 @@
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.9.2")]
-[assembly: AssemblyFileVersion("0.9.2")]
+[assembly: AssemblyVersion("1.0.1")]
+[assembly: AssemblyFileVersion("1.0.1")]
