d454b9e6340594ec1b88fd465dbd946c54e2f448
Amir Rajan <ar@amirrajan.net>
2015-10-04 16:42:20 -0600

    async support push to nuget attempt 3

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -21,5 +21,5 @@
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.9.69")]
-[assembly: AssemblyFileVersion("0.9.69")]
+[assembly: AssemblyVersion("0.9.1")]
+[assembly: AssemblyFileVersion("0.9.1")]
