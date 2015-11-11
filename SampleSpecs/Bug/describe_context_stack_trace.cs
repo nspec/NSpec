f5f8d8c3b24313af5911d16440cf8e2b407924c0
Amir Rajan <ar@amirrajan.net>
2015-11-11 11:42:57 -0600

    removed stack trace for expected output given new way of asserting output

--- SampleSpecs/Bug/describe_context_stack_trace.cs
+++ SampleSpecs/Bug/describe_context_stack_trace.cs
@@ -36,8 +36,6 @@ describe context stack trace
 
 nspec. describe context stack trace. exception thrown in act. is true.
 Context Failure: Exception in act., Example Failure: Expected: True, But was: False
-   at SampleSpecs.Bug.describe_context_stack_trace.MethodThrowsExceptionAndShouldBeInStackTrace() in SampleSpecs\Bug\describe_context_stack_trace.cs:line 24
-   at SampleSpecs.Bug.describe_context_stack_trace.<exception_thrown_in_act>b__0() in SampleSpecs\Bug\describe_context_stack_trace.cs:line 14
 
 1 Examples, 1 Failed, 0 Pending
 ";
