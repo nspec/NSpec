305ee9637845b7c2e67e281f97722a792b02293d
Giuseppe Piscopo <g.piscopo@braincrumbz.com>
2016-09-04 15:26:59 +0200

    feat(samples): add sample solution to try local dotnet test runner

---
+++ sln/test/Samples/DotNetTestSample/test/LibrarySpecs/describe_ConstantGenerator.cs
@@ -0,0 +1,29 @@
+﻿using LibraryUnderTest;
+using NSpec;
+
+namespace LibrarySpecs
+{
+    public class describe_ConstantGenerator : nspec
+    {
+        ConstantGenerator generator;
+
+        void before_each()
+        {
+            generator = new ConstantGenerator();
+        }
+
+        void Given_number_is_requested_twice()
+        {
+            int first = -1;
+            int second = -2;
+
+            act = () =>
+            {
+                first = generator.GetNumber();
+                second = generator.GetNumber();
+            };
+
+            it["Should return same number"] = () => first.should_be(second);
+        }
+    }
+}
