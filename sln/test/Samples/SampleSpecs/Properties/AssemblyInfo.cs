6b1673fa3df4ae09b9a3e8e9e11ee893c5effc0e
Giuseppe Piscopo <g.piscopo@braincrumbz.com>
2016-09-04 15:26:23 +0200

    chore(samples): restore standard AssemblyInfo

--- sln/test/Samples/SampleSpecs/Properties/AssemblyInfo.cs
+++ sln/test/Samples/SampleSpecs/Properties/AssemblyInfo.cs
@@ -1,12 +1,14 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SampleSpecs")]
-[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SampleSpecs")]
+[assembly: AssemblyTrademark("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
