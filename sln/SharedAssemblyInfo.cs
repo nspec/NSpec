1e3f29bd73475fe9930e56851f9cc4ed2ca8315e
Giuseppe Piscopo <g.piscopo@braincrumbz.com>
2016-11-07 23:15:47 +0100

    chore(publish): setup dotnet pack, revert to sensible version nr
    **NOTE**: currently, dotnet pack seems to ignore copying contents other
    than project output

--- sln/SharedAssemblyInfo.cs
+++ sln/SharedAssemblyInfo.cs
@@ -21,6 +21,6 @@ using System.Reflection;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("2.0.0")]
-[assembly: AssemblyFileVersion("2.0.0")]
+[assembly: AssemblyVersion("1.1.0")]
 
+// AssemblyFileVersion not explicitly specified, so it will be same as AssemblyVersion
