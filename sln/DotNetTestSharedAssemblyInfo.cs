726b7295dc900d7377993f1ffbab2295a4233160
GiuseppePiscopo <GiuseppePiscopo@users.noreply.github.com>
2017-02-02 16:50:39 +0100

    chore(build): dotnet-test runner has distinct assembly info

---
+++ sln/DotNetTestSharedAssemblyInfo.cs
@@ -0,0 +1,26 @@
+using System.Reflection;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("dotnet-test-nspec")]
+[assembly: AssemblyCopyright("Copyright (c) 2016 BrainCrumbz, The NSpec Development Team")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+
+[assembly: AssemblyVersion("0.1.0")]
+
+// AssemblyFileVersion not explicitly specified, so it will be same as AssemblyVersion
