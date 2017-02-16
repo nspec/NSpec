30038bdb3b92e5a7891b624b2d721da36012ab15
GiuseppePiscopo <GiuseppePiscopo@users.noreply.github.com>
2017-02-16 16:44:38 +0100

    test(api): unit test Api.Discovery.ExampleSelector

---
+++ sln/test/NSpec.Tests/Api/ApiTestData.cs
@@ -0,0 +1,107 @@
+﻿using NSpec.Api.Discovery;
+using System.Reflection;
+
+namespace NSpec.Tests.Api
+{
+    public static class ApiTestData
+    {
+        static ApiTestData()
+        {
+            foreach (var exm in allDiscoveredExamples)
+            {
+                exm.SourceAssembly = testAssemblyPath;
+            }
+        }
+
+        public static readonly string testAssemblyPath =
+            typeof(SampleSpecsApi.DummyPublicClass).GetTypeInfo().Assembly.Location;
+
+        public static readonly DiscoveredExample[] allDiscoveredExamples =
+        {
+            new DiscoveredExample()
+            {
+                FullName = "nspec. ParentSpec. method context 1. parent example 1A.",
+                SourceFilePath = "",
+                SourceLineNumber = 0,
+                Tags = new[]
+                {
+                    "Tag-1A",
+                    "Tag-1B",
+                    "ParentSpec",
+                },
+            },
+            new DiscoveredExample()
+            {
+                FullName = "nspec. ParentSpec. method context 1. parent example 1B.",
+                SourceFilePath = "",
+                SourceLineNumber = 0,
+                Tags = new[]
+                {
+                    "Tag-1A",
+                    "Tag-1B",
+                    "ParentSpec",
+                },
+            },
+            new DiscoveredExample()
+            {
+                FullName = "nspec. ParentSpec. method context 2. parent example 2A.",
+                SourceFilePath = "",
+                SourceLineNumber = 0,
+                Tags = new[]
+                {
+                    "ParentSpec",
+                },
+            },
+            new DiscoveredExample()
+            {
+                FullName = "nspec. ParentSpec. ChildSpec. method context 3. child example 3A skipped.",
+                SourceFilePath = "",
+                SourceLineNumber = 0,
+                Tags = new[]
+                {
+                    "Tag-Child-example-skipped",
+                    "Tag-Child",
+                    "ChildSpec",
+                    "ParentSpec",
+                },
+            },
+            new DiscoveredExample()
+            {
+                FullName = "nspec. ParentSpec. ChildSpec. method context 4. child example 4A.",
+                SourceFilePath = "",
+                SourceLineNumber = 0,
+                Tags = new[]
+                {
+                    "Tag with underscores",
+                    "Tag-Child",
+                    "ChildSpec",
+                    "ParentSpec",
+                },
+            },
+            new DiscoveredExample()
+            {
+                FullName = "nspec. ParentSpec. ChildSpec. method context 5. sub context 5-1. child example 5-1A failing.",
+                SourceFilePath = "",
+                SourceLineNumber = 0,
+                Tags = new[]
+                {
+                    "Tag-Child",
+                    "ChildSpec",
+                    "ParentSpec",
+                },
+            },
+            new DiscoveredExample()
+            {
+                FullName = "nspec. ParentSpec. ChildSpec. method context 5. sub context 5-1. child example 5-1B.",
+                SourceFilePath = "",
+                SourceLineNumber = 0,
+                Tags = new[]
+                {
+                    "Tag-Child",
+                    "ChildSpec",
+                    "ParentSpec",
+                },
+            },
+        };
+    }
+}
---
+++ sln/test/NSpec.Tests/Api/Discovery/describe_ExampleSelector.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using NSpec.Api.Discovery;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace NSpec.Tests.Api.Discovery
+{
+    [TestFixture]
+    [Category("ExampleSelector")]
+    public class describe_ExampleSelector
+    {
+        ExampleSelector selector;
+
+        IEnumerable<DiscoveredExample> actuals;
+
+        readonly string testAssemblyPath;
+
+        public describe_ExampleSelector()
+        {
+            testAssemblyPath = ApiTestData.testAssemblyPath;
+        }
+
+        [SetUp]
+        public void setup()
+        {
+            selector = new ExampleSelector();
+
+            actuals = selector.Select(testAssemblyPath);
+        }
+
+        [Test]
+        public void it_should_return_discovered_examples()
+        {
+            var expecteds = ApiTestData.allDiscoveredExamples;
+
+            actuals.ShouldBeEquivalentTo(expecteds);
+        }
+    }
+}
