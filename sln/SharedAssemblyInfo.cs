7b0e276fed3dc2769282b655acf726450ef671c6
GiuseppePiscopo <GiuseppePiscopo@users.noreply.github.com>
2017-03-06 13:05:48 +0100

    chore(package): up version to 3.0.3
    Also, drop using AppVeyor private project feed

--- sln/SharedAssemblyInfo.cs
+++ sln/SharedAssemblyInfo.cs
@@ -21,6 +21,6 @@ using System.Reflection;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("3.0.2")]
+[assembly: AssemblyVersion("3.0.3")]
 
 // AssemblyFileVersion not explicitly specified, so it will be same as AssemblyVersion
