73625cbfcec5b636a7ba18bcd0881ce6cf45c316
GiuseppePiscopo <GiuseppePiscopo@users.noreply.github.com>
2017-03-21 18:34:13 +0100

    chore(package): up version to 3.0.4

--- sln/SharedAssemblyInfo.cs
+++ sln/SharedAssemblyInfo.cs
@@ -21,6 +21,6 @@ using System.Reflection;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("3.0.3")]
+[assembly: AssemblyVersion("3.0.4")]
 
 // AssemblyFileVersion not explicitly specified, so it will be same as AssemblyVersion
