c9d68d1aef360811f4b258cb61027849356739e3
Giuseppe Piscopo <braincrumbz@Mac-mini-di-BrainCrumbz.local>
2017-05-17 23:28:39 +0200

    test(asyncAfter): example should run but fail when after throws
    Also, preserve example exception when after throws

--- sln/test/NSpec.Tests/WhenRunningSpecs/Exceptions/when_async_after_contains_exception.cs
+++ sln/test/NSpec.Tests/WhenRunningSpecs/Exceptions/when_async_after_contains_exception.cs
@@ -2,6 +2,8 @@
 using NUnit.Framework;
 using System.Threading.Tasks;
 using FluentAssertions;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace NSpec.Tests.WhenRunningSpecs.Exceptions
 {
@@ -20,38 +22,68 @@ namespace NSpec.Tests.WhenRunningSpecs.Exceptions
                     throw new AfterException();
                 };
 
-                it["should fail this example because of afterAsync"] = () => Assert.That(true, Is.True);
+                it["should fail this example because of afterAsync"] = () =>
+                {
+                    ExamplesRun.Add("should fail this example because of afterAsync");
+                    Assert.That(true, Is.True);
+                };
 
-                it["should also fail this example because of afterAsync"] = () => Assert.That(true, Is.True);
+                it["should also fail this example because of afterAsync"] = () =>
+                {
+                    ExamplesRun.Add("should also fail this example because of afterAsync");
+                    Assert.That(true, Is.True);
+                };
 
-                it["overrides exception from same level it"] = () => { throw new ItException(); };
+                it["preserves exception from same level it"] = () =>
+                {
+                    ExamplesRun.Add("preserves exception from same level it");
+                    throw new ItException();
+                };
 
                 context["exception thrown by both afterAsync and nested before"] = () =>
                 {
                     before = () => { throw new BeforeException(); };
 
-                    it["preserves exception from nested before"] = () => Assert.That(true, Is.True);
+                    it["preserves exception from nested before"] = () =>
+                    {
+                        ExamplesRun.Add("preserves exception from nested before");
+                        Assert.That(true, Is.True);
+                    };
                 };
 
                 context["exception thrown by both afterAsync and nested act"] = () =>
                 {
                     act = () => { throw new ActException(); };
 
-                    it["preserves exception from nested act"] = () => Assert.That(true, Is.True);
+                    it["preserves exception from nested act"] = () =>
+                    {
+                        ExamplesRun.Add("preserves exception from nested act");
+                        Assert.That(true, Is.True);
+                    };
                 };
 
                 context["exception thrown by both afterAsync and nested it"] = () =>
                 {
-                    it["overrides exception from nested it"] = () => { throw new ItException(); };
+                    it["preserves exception from nested it"] = () => 
+                    {
+                        ExamplesRun.Add("preserves exception from nested it");
+                        throw new ItException();
+                    };
                 };
 
                 context["exception thrown by both afterAsync and nested after"] = () =>
                 {
-                    it["preserves exception from nested after"] = () => Assert.That(true, Is.True);
+                    it["preserves exception from nested after"] = () =>
+                    {
+                        ExamplesRun.Add("preserves exception from nested after");
+                        Assert.That(true, Is.True);
+                    };
 
                     after = () => { throw new AfterException(); };
                 };
             }
+
+            public static List<string> ExamplesRun = new List<string>();
         }
 
         [SetUp]
@@ -63,36 +95,32 @@ namespace NSpec.Tests.WhenRunningSpecs.Exceptions
         [Test]
         public void the_example_level_failure_should_indicate_a_context_failure()
         {
-            TheExample("should fail this example because of afterAsync")
-                .Exception.Should().BeOfType<ExampleFailureException>();
-            TheExample("should also fail this example because of afterAsync")
-                .Exception.Should().BeOfType<ExampleFailureException>();
-            TheExample("overrides exception from same level it")
-                .Exception.Should().BeOfType<ExampleFailureException>();
-            TheExample("preserves exception from nested before")
-                .Exception.Should().BeOfType<ExampleFailureException>();
-            TheExample("preserves exception from nested act")
-                .Exception.Should().BeOfType<ExampleFailureException>();
-            TheExample("overrides exception from nested it")
-                .Exception.Should().BeOfType<ExampleFailureException>();
-            TheExample("preserves exception from nested after")
-                .Exception.Should().BeOfType<ExampleFailureException>();
+            classContext.AllExamples()
+                .Where(e => !new []
+                {
+                    "preserves exception from same level it",
+                    "preserves exception from nested it",
+                }.Contains(e.Spec))
+                .Should().OnlyContain(e => e.Exception is ExampleFailureException);
         }
 
         [Test]
         public void examples_with_only_after_async_failure_should_fail_because_of_after_async()
         {
-            TheExample("should fail this example because of afterAsync")
-                .Exception.InnerException.Should().BeOfType<AfterException>();
-            TheExample("should also fail this example because of afterAsync")
-                .Exception.InnerException.Should().BeOfType<AfterException>();
+            classContext.AllExamples()
+                .Where(e => new []
+                {
+                    "should fail this example because of afterAsync",
+                    "should also fail this example because of afterAsync",
+                }.Contains(e.Spec))
+                .Should().OnlyContain(e => e.Exception.InnerException is AfterException);
         }
 
         [Test]
-        public void it_should_throw_exception_from_after_async_not_from_same_level_it()
+        public void it_should_throw_exception_from_same_level_it_not_from_after_async()
         {
-            TheExample("overrides exception from same level it")
-                .Exception.InnerException.Should().BeOfType<AfterException>();
+            TheExample("preserves exception from same level it")
+                .Exception.Should().BeOfType<ItException>();
         }
 
         [Test]
@@ -110,10 +138,10 @@ namespace NSpec.Tests.WhenRunningSpecs.Exceptions
         }
 
         [Test]
-        public void it_should_throw_exception_from_after_async_not_from_nested_it()
+        public void it_should_throw_exception_from_nested_it_not_from_after_async()
         {
-            TheExample("overrides exception from nested it")
-                .Exception.InnerException.Should().BeOfType<AfterException>();
+            TheExample("preserves exception from nested it")
+                .Exception.Should().BeOfType<ItException>();
         }
 
         [Test]
@@ -121,6 +149,28 @@ namespace NSpec.Tests.WhenRunningSpecs.Exceptions
         {
             TheExample("preserves exception from nested after")
                 .Exception.InnerException.Should().BeOfType<AfterException>();
+        }
+
+        [Test]
+        public void examples_should_fail_for_formatter()
+        {
+            formatter.WrittenExamples.Should().OnlyContain(e => e.Failed);
+        }
+
+        [Test]
+        public void examples_body_should_still_run()
+        {
+            string[] expecteds = new[]
+            {
+                "should fail this example because of afterAsync",
+                "should also fail this example because of afterAsync",
+                "preserves exception from same level it",
+                "preserves exception from nested act",
+                "preserves exception from nested it",
+                "preserves exception from nested after",
+            };
+
+            AsyncAfterThrowsSpecClass.ExamplesRun.ShouldBeEquivalentTo(expecteds);
         }
     }
 }
